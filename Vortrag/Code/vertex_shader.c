cbfc15d4b2176ccdddd98fe0fb6dbfb4e8d000f1
Jones <jonasheinle@googlemail.com>
2019-06-24 15:09:35 +0200

    vertex shader verbessert

--- Vortrag/Code/vertex_shader.c
+++ Vortrag/Code/vertex_shader.c
@@ -2,10 +2,15 @@ uniform mat4 MVP , MV , Normale;
 uniform vec3 lightSourcePosition;
 in vec4 position;
 in vec3 normal;
+//Gourand Shading
+out vec4 color;
+//nur falls kein Gourand Shading
 out vec3 L, N;
 void main ()
     gl_Position = MVP * position;
     vec3 P = vec3(matrixMV * position);
-    N = vec3(Normale * vec4(in_normal,0.0));
-    L = vec3(lightSourcePos) - P;
+    N = normalize(vec3(Normale * vec4(in_normal,0.0)));
+    L = vec3((lightSourcePos) - P);
+    //nur falls Gourand Shading
+    color = vec4(max(0.0, dot(L,N)));
 }
